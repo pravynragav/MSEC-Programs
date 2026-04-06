a2f627fe783a0c4275fceb8130e560804a86117e
pravynragav <167666347+pravynragav@users.noreply.github.com>
2026-04-06 11:31:54 +0530

    Add Stringstream.cpp for parsing integers from string

---
+++ Hackerrank/C++/Stringstream.cpp
@@ -0,0 +1,27 @@
+#include <sstream>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> parseInts(string str) 
+{
+stringstream s(str);
+vector <int> a;
+char ch;
+int t;
+while(s>>t)
+{ a.push_back(t);
+s>>ch;}
+return a;
+}
+
+int main() {
+    string str;
+    cin >> str;
+    vector<int> integers = parseInts(str);
+    for(int i = 0; i < integers.size(); i++) {
+        cout << integers[i] << endl;
+    }
+    
+    return 0;
+}
